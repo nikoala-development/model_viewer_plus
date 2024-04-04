2bcd051fd259a2429ad6b0b8f5afb77851415867
Nikola Hack <34539683+bynikoala@users.noreply.github.com>
2024-04-04 13:30:38 +0200

    introducing skybox-height

--- lib/src/model_viewer_plus_web.dart
+++ lib/src/model_viewer_plus_web.dart
@@ -106,6 +106,7 @@ class ModelViewerState extends State<ModelViewer> {
       interpolationDecay: widget.interpolationDecay,
       // Lighting & Env Attributes
       skyboxImage: widget.skyboxImage,
+      skyboxHeight: widget.skyboxHeight,
       environmentImage: widget.environmentImage,
       exposure: widget.exposure,
       shadowIntensity: widget.shadowIntensity,
